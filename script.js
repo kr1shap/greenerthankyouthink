dd2b967a7b5b20c1ee5f3619ad0d617d40be3c2c
Krisha Patel <krishapatel@Krishas-MacBook-Air.local>
2025-02-15 17:03:40 -0500

    Initial commit of website files

---
+++ script.js
@@ -0,0 +1,39 @@
+function setMd(event) { 
+    let moods = { 
+        m1: "Your mood: ( ˶ˆ꒳ˆ˵ )",
+        m2: "Your mood: (ᵕ—_—)",
+        m3: "Your mood: (っ◞‸◟ c)",
+        m4: "Your mood: ( ｡ •̀ ᴖ •́ ｡)💢"
+    }
+
+    let clickedButton = event.target.id; // Get the ID of the clicked button
+    let mood = moods[clickedButton]; 
+    document.getElementById("mood-txt").innerText = mood; // Update text
+}
+
+
+function changeTxt(event) {
+    let plantNames = {
+        p1: "Happi happi plant(*ᴗ͈ˬᴗ͈)ꕤ*.ﾟ",
+        p2: "Depres Plant (っ◞‸◟ c)",
+        p3: "Kirby Plant (˶˃ ᵕ ˂˶) .ᐟ.",
+        p4: "Woozy doozy Plant ( ꩜ ᯅ ꩜;)⁭ ⁭",
+        p5: "Euphoria ᓚ₍ ^. .^₎"
+    };
+
+    let messages = {
+        p1: "You deserve the love you so freely give to others",
+        p2: "Did you know, women are 50% more likely to be depressed than men; so lets promote equality!",
+        p3: "You are not your thoughts. You are the observer of them",
+        p4: "Feelings are just visitors. Let them come and go",
+        p5: "Turn your anger around, did you know anger can be a source of motivation?"
+    };
+
+    let clickedButton = event.target.id; // Get the ID of the clicked button
+    let message = messages[clickedButton] || "You planted a mystery plant! 🪴"; // Default message
+    let plantName = plantNames[clickedButton] || "You planted a mystery plant! 🪴"; // Default message
+
+    document.getElementById("pFact").innerText = message; // Update text
+    document.getElementById("plant-tit").innerText = plantName; // Update text
+
+}
